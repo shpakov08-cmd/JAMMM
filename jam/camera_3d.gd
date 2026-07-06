c7c5cc67dba2ff6c7c464dd1651559c81455dd94
mocha_hacha <tqwer6576@gmail.com>
2026-07-06 17:33:42 +0300

    camera_updata

--- jam/camera_3d.gd
+++ jam/camera_3d.gd
@@ -2,21 +2,49 @@ extends Camera3D
 
 @export var sensitivity := 0.003
 
+# зум переменные
+@export var min_fov := 20.0
+@export var max_fov := 75.0
+@export var zoom_step := 3.0
+@export var zoom_speed := 5.0
 var pitch := 0.0
+var target_fov := 75.0
+
+
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	fov = max_fov
+	target_fov = max_fov
+
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		
+		# поворот 
+		get_parent().rotate_y(-event.relative.x * sensitivity)
+
+		# Поворот камеры вверх-вниз
 		pitch -= event.relative.y * sensitivity
-		pitch = clamp(pitch, deg_to_rad(-89), deg_to_rad(89))
+		pitch = clamp(
+			pitch,
+			deg_to_rad(-89),
+			deg_to_rad(89)
+		)
 		rotation.x = pitch
 
-		
-		if get_parent():
-			get_parent().rotate_y(-event.relative.x * sensitivity)
+	# зум
+	elif event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			target_fov -= zoom_step
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			target_fov += zoom_step
 
-	if event.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		target_fov = clamp(
+			target_fov,
+			min_fov,
+			max_fov
+		)
+
+func _process(delta):
+	fov = lerp(fov, target_fov, zoom_speed * delta)
