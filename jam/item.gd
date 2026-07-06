ce29379c6e35e277ddb47e5330c1b23d423f15cf
shpakov08-cmd <shpakov08@internet.ru>
2026-07-06 18:14:13 +0300

    Revert "item_update3"
    This reverts commit e7b13e96f73d5b228d2062fa87467ecc50d5d44d.

--- jam/item.gd
+++ jam/item.gd
@@ -1,26 +1,26 @@
 extends StaticBody3D
 
 var mesh_node: MeshInstance3D
-var original_material: Material
-var transparent_material: StandardMaterial3D
+var original_material: StandardMaterial3D
+var highlight_material: StandardMaterial3D
 var is_highlighted = false
 
 func _ready():
-	mesh_node = $Mesh
+	mesh_node = $Mesh 
 	
-	if mesh_node:
-		original_material = mesh_node.get_surface_override_material(0)
-		if original_material == null:
-			original_material = mesh_node.mesh.surface_get_material(0)
-		
-		transparent_material = StandardMaterial3D.new()
-		transparent_material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
-		transparent_material.albedo_color = Color(1, 1, 1, 0.5)  
+
+	highlight_material = StandardMaterial3D.new()
+	highlight_material.emission_enabled = true
+	highlight_material.emission = Color(1.0, 0.933, 1.0, 1.0)
+	highlight_material.emission_energy_multiplier = 2.0
+	
+	if mesh_node and mesh_node.mesh:
+		original_material = mesh_node.get_active_material(0)
 
 func highlight():
 	if not is_highlighted and mesh_node:
 		is_highlighted = true
-		mesh_node.set_surface_override_material(0, transparent_material)
+		mesh_node.set_surface_override_material(0, highlight_material)
 
 func unhighlight():
 	if is_highlighted and mesh_node:
@@ -29,4 +29,4 @@ func unhighlight():
 
 func pick_up():
 	print("Предмет подобран: ", name)
-	queue_free()
+	queue_free() 
