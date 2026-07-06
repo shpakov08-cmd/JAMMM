749cdc8b377a754f4cbfe5394fd8794005281924
mocha_hacha <tqwer6576@gmail.com>
2026-07-06 17:01:28 +0300

    camera

--- jam/camera_3d.gd
+++ jam/camera_3d.gd
@@ -1,1 +1,22 @@
 extends Camera3D
+
+@export var sensitivity := 0.003
+
+var pitch := 0.0
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		
+		pitch -= event.relative.y * sensitivity
+		pitch = clamp(pitch, deg_to_rad(-89), deg_to_rad(89))
+		rotation.x = pitch
+
+		
+		if get_parent():
+			get_parent().rotate_y(-event.relative.x * sensitivity)
+
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
